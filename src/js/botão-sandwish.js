58d69d5a252ad50e006e8f261a8aca5285c33d4e
SamColt36 <samuelss.contato@gmail.com>
2023-09-19 20:59:27 -0300

    Ajustes no arquivo botão-sandwish.js

--- src/js/botão-sandwish.js
+++ src/js/botão-sandwish.js
@@ -1,34 +1,47 @@
-const menuSuspenso = document.getElementById('menuSuspenso')
-const closeMenu = document.getElementById('closeMenu')
-const btnSandwish = document.getElementById('btnSandwish')
-const pathElement = document.getElementById("btnSandwishSvgPath");
-const spanBlack = document.getElementById('spanBlack')
+const menuSuspenso = document.querySelectorAll('.menuSuspenso')
+const closeMenu = document.querySelectorAll('.closeMenu')
+const btnSandwish = document.querySelectorAll('.btnSandwish')
+const pathElement = document.querySelectorAll('.btnSandwishSvgPath')
+const spanBlack = document.querySelectorAll('.spanBlack')
 
-// Parâmetro que gera o icon menu sandwish ativado
 const pathParamDactived = 'M3.75 9h16.5m-16.5 6.75h16.5'
-// Parâmetro que gera o icon menu sandwish desativado
 const pathParamDdisabled = 'M6 18L18 6M6 6l12 12'
 let count = 1
 
-btnSandwish.classList.add('transition', 'duration-700', 'transition-all', 'ease-in-out')
-menuSuspenso.classList.add('transition', 'duration-700', 'transition-all', 'ease-in-out')
+btnSandwish.forEach(e => e.classList.add('transition', 'duration-700', 'transition-all', 'ease-in-out'))
+
+menuSuspenso.forEach(e => e.classList.add('transition', 'duration-700', 'transition-all', 'ease-in-out'))
 
 function ativarMenu() {
-	pathElement.setAttribute("d", pathParamDactived);
-	spanBlack.classList.remove('fixed');
-	spanBlack.classList.add('hidden');
-	btnSandwish.classList.remove('text-white');
-	btnSandwish.classList.add('text-black');
-	menuSuspenso.classList.add('translate-y-full');
+	pathElement.forEach(e => e.setAttribute("d", pathParamDactived))
+
+	spanBlack.forEach(e => {
+		e.classList.remove('fixed')
+		e.classList.add('hidden')
+	})
+
+	btnSandwish.forEach(e => {
+		e.classList.remove('text-white')
+		e.classList.add('text-black')
+	})
+
+	menuSuspenso.forEach(e => e.classList.add('translate-y-full'))
 }
 
 function desativarMenu() {
-	pathElement.setAttribute("d", pathParamDdisabled);
-	spanBlack.classList.remove('hidden');
-	spanBlack.classList.add('fixed');
-	btnSandwish.classList.remove('text-black');
-	btnSandwish.classList.add('text-white');
-	menuSuspenso.classList.remove('translate-y-full');
+	pathElement.forEach(e => e.setAttribute("d", pathParamDdisabled))
+
+	spanBlack.forEach(e => {
+		e.classList.remove('hidden')
+		e.classList.add('fixed')
+	})
+
+	btnSandwish.forEach(e => {
+		e.classList.remove('text-black')
+		e.classList.add('text-white')
+	})
+
+	menuSuspenso.forEach(e => e.classList.remove('translate-y-full'))
 }
 
 function comportamentoBtnMenuSuspenso() {
@@ -36,12 +49,6 @@ function comportamentoBtnMenuSuspenso() {
 	return (count % 2 === 0) ? desativarMenu() : ativarMenu()
 }
 
-btnSandwish.addEventListener('click', () => {
-	comportamentoBtnMenuSuspenso()
-})
+btnSandwish.forEach(e => e.addEventListener('click', () => comportamentoBtnMenuSuspenso()))
 
-
-closeMenu.addEventListener('click', () => {
-	comportamentoBtnMenuSuspenso()
-})
-
+closeMenu.forEach(elemento => elemento.addEventListener('click', () => comportamentoBtnMenuSuspenso()))
