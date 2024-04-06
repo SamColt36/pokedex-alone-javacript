07a7a47c770ff8957c6467ce63203709b7df03cf
SamColt36 <operator.samuelcolt@gmail.com>
2024-04-06 19:07:26 -0300

    fix(bug): redirecionamento e links de redes sociais

--- src/js/main-details.js
+++ src/js/main-details.js
@@ -55,7 +55,7 @@ const mainDetail = ({ name, id, types, sprites }) => {
 		<!--/Esse span é o responsável por deixar a tela escura quando o menu suspenso está ativado-->
 		<nav class="mb-32 flex justify-between items-center p-4 ${type}" id="nav">
 			<div class="flex space-x-3">
-				<a href="../../index.html">
+				<a href="">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
 						stroke="currentColor" class="w-7 h-7 text-white drop-shadow-xl cursor-pointer">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
